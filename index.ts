317b933a8725822a86bfe3fbea334acb9529e425
lakshman-riktamtech <lakshmana@riktamtech.com>
2025-06-14 03:10:58 +0530

    rate-limit-added

--- index.ts
+++ index.ts
@@ -3,19 +3,45 @@ import cors from 'cors';
 import multer from 'multer';
 import dotenv from 'dotenv';
 import { getResumeReviewFromFile, getJobMatchFromFileAndDescription } from './services/gemini.service';
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5,                 // Limit each IP to 100 requests per window
+  message: 'Too many requests from this IP, please try again later.',
+  standardHeaders: true,    // `RateLimit-*` headers
+  legacyHeaders: false,     // Disable `X-RateLimit-*` headers
+  skip : (req: Request, res: Response) => { 
+    return req.method === "GET" || req.path === "/";
+  }
+});
+
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-const upload = multer({ storage: multer.memoryStorage() });
 
+app.set('trust proxy', 1); // <-- Add this line here
+
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 10 * 1024 * 1024 } // 10MB limit
+});
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(limiter);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('IP:', req.ip);
+  next();
+});
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from the backend!');
